9d2ba953afdb0988684b951f3d704fdd2bf87135
Credit-Currency <jhamiltonactually@gmail.com>
2014-02-01 02:50:28 +0000

    Renamed to Credit, altered mining characteristics

--- src/net.cpp
+++ src/net.cpp
@@ -996,7 +996,7 @@ void ThreadSocketHandler2(void* parg)
 void ThreadMapPort(void* parg)
 {
     IMPLEMENT_RANDOMIZE_STACK(ThreadMapPort(parg));
-
+	//LINE 1000  {"some website name", "somewebsite.org or ip x.x.x.x"} you can set an initial seed node or always on system that the new coin wallets coming online will check for additional addresses: here
     // Make this thread recognisable as the UPnP thread
     RenameThread("bitcoin-UPnP");
 
@@ -1061,7 +1061,7 @@ void ThreadMapPort2(void* parg)
             }
         }
 
-        string strDesc = "SmallChange " + FormatFullVersion();
+        string strDesc = "Credit " + FormatFullVersion();
 #ifndef UPNPDISCOVER_SUCCESS
         /* miniupnpc 1.5 */
         r = UPNP_AddPortMapping(urls.controlURL, data.first.servicetype,
@@ -1727,7 +1727,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. SmallChange is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. Credit is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
